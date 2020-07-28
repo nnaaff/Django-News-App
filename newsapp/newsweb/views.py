343c4993d50cd392f8f0862bb9ce3f57fd836ab3
Nafisa <nafisee@gmail.com>
2020-07-29 00:13:18 +0530

    Added basic Bootstrap styling

--- newsapp/newsweb/views.py
+++ newsapp/newsweb/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'newsweb/home.html', {})
+    import requests
+    import json
+
+    news_api_req = requests.get("http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4cbf5645264840fb96e4c5695ee6e312")
+    api = json.loads(news_api_req.content)
+    return render(request, 'newsweb/home.html', {'api': api})
 
 
